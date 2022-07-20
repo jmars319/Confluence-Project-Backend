036e63e72a8fb8412dbdb487f4f3044ce6aa8bd9
Jason <jam319@protonmail.com>
2022-07-20 19:18:06 -0400

    added first endpoint and listener

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ import express from 'express'
 import mongoose from 'mongoose'
 
 //App Config
+const app = express();
+const port = process.env.PORT || 3000
 
 //Middleware
 
 //DB Config
 
 //API Endpoints
+app.get('/', (req, res) => res.status(200).send("HELLO CLEVER PROGRAMMERS!!!"));
 
 //Listener
+app.listen(port, () => console.log(`listening on localhost: ${port}`));
