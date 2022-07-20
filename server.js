d8a12265923ce4b9e93e4837bdf75f3d358a0976
Jason <jam319@protonmail.com>
2022-07-20 19:31:18 -0400

    added DB config parameters and db card schema

--- server.js
+++ server.js
@@ -4,10 +4,16 @@ import mongoose from 'mongoose'
 //App Config
 const app = express();
 const port = process.env.PORT || 3000
+const connectionURL = 'mongodb+srv://admin:S4aA2KjIJNPpxipW@cluster0.mxm98.mongodb.net/confluencedb?retryWrites=true&w=majority'
 
 //Middleware
 
 //DB Config
+mongoose.connect(connectionURL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useInifiedTopology: true,
+})
 
 //API Endpoints
 app.get('/', (req, res) => res.status(200).send("HELLO CLEVER PROGRAMMERS!!!"));
